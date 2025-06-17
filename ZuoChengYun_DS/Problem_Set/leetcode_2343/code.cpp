a7a2c4e5e3951fc55fd83edaf7fad2dae983884d
CodeMan1729 <1663886652@qq.com>
2025-06-17 20:47:10 +0800

    first day use anki

--- ZuoChengYun_DS/Problem_Set/leetcode_2343/code.cpp
+++ ZuoChengYun_DS/Problem_Set/leetcode_2343/code.cpp
@@ -1,44 +1,61 @@
-class Solution {
+#include <vector>
+#include <string>
+
+using namespace std;
+
+// 找到 nums 中第 ki 小数字对应的 下标 <----> 基数排序有稳定性
+
+class Solution 
+{
+private:
+    size_t BASE = 10; // 因为题目的字符串数组时10进位！
+
 public:
-    vector<int> smallestTrimmedNumbers(vector<string>& nums, vector<vector<int>>& queries) {
-        int n = nums.size();         // nums 的数量
-        int m = nums[0].size();      // 每个字符串的长度
-        vector<vector<int>> vecs(m + 1); // 存储每轮排序后的下标
-        
-        // 初始化第 0 轮，所有 nums 的下标
-        for (int i = 0; i < n; i++) {
-            vecs[0].push_back(i);
+    vector<int> smallestTrimmedNumbers(vector<string>& nums, vector<vector<int>>& queries) 
+    {
+        // 搞清楚本次字符数字有几位
+        size_t n = nums[0].length();
+        size_t nums_size = nums.size();
+
+        vector<vector<int>> bucket(BASE);
+        vector<vector<int>> ans_list(n + 1); // 装入nums的下标
+
+        // 第0次排序放nums
+        for(int i = 0; i < nums_size; i++)
+        {
+            ans_list[0].push_back(i);
         }
 
-        // 逐位进行基数排序，从低位到高位
-        for (int i = 1; i <= m; i++) 
+        // 基数排序+存储每轮结果
+        for(int i = 1; i <= n; i++)
         {
-            vector<vector<int>> buckets(10); // 10 个桶，分别对应数字 0-9
-
-            // 根据当前位（从右边第 i 位）将下标放入桶中
-            for (int idx : vecs[i - 1]) 
+            // 从左到右遍历，装入桶
+            for(auto& elem : ans_list[i - 1]) // item为nums的某一位的下标
             {
-                int digit = nums[idx][m - i] - '0'; // 提取当前位的数字 + 两个技巧
-                buckets[digit].push_back(idx);
+                int digit = nums[elem][n - i] - '0';
+                bucket[digit].push_back(elem);
             }
 
-            // 将桶中的下标按顺序合并到 vecs[i]
-            for (int j = 0; j < 10; j++) 
+            // 依次从桶里倒出
+            for(int j = 0; j < BASE; j++)
             {
-                for (int idx : buckets[j]) 
+                for(auto& elem : bucket[j])
                 {
-                    vecs[i].push_back(idx);
+                    ans_list[i].push_back(elem);
                 }
+                bucket[j].clear();
             }
         }
 
-        // 处理查询
+        // 查询即可
+        size_t queries_size = queries.size();
         vector<int> answer;
-        for (auto& q : queries) 
+        for(auto& elem : queries)
         {
-            int k = q[0];        // 第 k 小
-            int trim_bit = q[1]; // 裁剪位数
-            answer.push_back(vecs[trim_bit][k - 1]); // 从排序结果中取第 k-1 个下标
+            int k = elem[0];
+            int trim = elem[1];
+
+            answer.push_back(ans_list[trim][k - 1]);
         }
 
         return answer;
